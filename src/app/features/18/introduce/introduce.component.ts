047743dade806cf7ab33fe855caf3b01d6348a0b
Kid Bui quang <kidbuiquang@Kids-MacBook-Pro.local>
2025-03-08 19:38:15 +0700

    [feature] add color for header and footer and for introduce version 18

--- src/app/features/18/introduce/introduce.component.ts
+++ src/app/features/18/introduce/introduce.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @Component({
 	selector: 'app-18-introduce',
-	imports: [],
+	imports: [FieldsetModule],
 	templateUrl: './introduce.component.html',
 	styleUrl: './introduce.component.scss',
 })
