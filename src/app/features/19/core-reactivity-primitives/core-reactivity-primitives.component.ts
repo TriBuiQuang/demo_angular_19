15aa807dcbd6814e3ce577ba05837db7be9ff79c
Kid Bui quang <kidbuiquang@Kids-MacBook-Pro.local>
2025-03-30 18:33:32 +0700

    [feature] Done Resource API of angular

--- src/app/features/19/core-reactivity-primitives/core-reactivity-primitives.component.ts
+++ src/app/features/19/core-reactivity-primitives/core-reactivity-primitives.component.ts
@@ -1,5 +1,6 @@
-import { Component, linkedSignal, signal } from '@angular/core';
+import { Component, linkedSignal, resource, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import axios from 'axios';
 
 import { FieldsetModule } from 'primeng/fieldset';
 import { TabsModule } from 'primeng/tabs';
@@ -21,11 +22,45 @@ export class CoreReactivityPrimitivesComponent {
 	imgSrcLinkedSignalExample = './assets/img/reactivity_linked_signal.png';
 	imgSrcResourceExample = './assets/img/reactivity_resource.png';
 
+	//#region Linked Signal
 	options = signal(['apple', 'banana', 'fig']);
 	currentTab = signal(0);
 
 	// Choice defaults to the first option, but can be changed.
 	choice = linkedSignal(() => this.options()[0]);
+	//#endregion Linked Signal
+
+	//#region Resource
+	query = signal('');
+
+	// Can be do this with resource
+	// products = resource({
+	// 	loader: async () => {
+	// 		const products = await fetch('http://localhost:3000/api/products');
+	// 		return products.json();
+	// 	},
+	// });
+
+	products = resource<any[], { query: string }>({
+		request: () => ({ query: this.query() }),
+		loader: async ({ request, abortSignal }) => {
+			console.log('request : ', request);
+			let filter = request.query ? '?name=' + request.query : '';
+
+			const products = await axios.get(`http://localhost:3000/api/products${filter}`, {
+				signal: abortSignal,
+			});
+			// .catch((error) => {
+			// 	console.error('error : ', error);
+			// 	return { data: [] };
+			// });
+
+			console.log('products : ', products.data.data);
+			return products.data.data;
+		},
+	});
+
+	//#endregion Resource
 
 	onClickSetLinkSignal = () => {
 		console.log(this.choice());
@@ -43,4 +78,12 @@ export class CoreReactivityPrimitivesComponent {
 		console.log('onChange tab', index, typeof index);
 		this.currentTab.set(index);
 	};
+
+	addProduct() {
+		console.log('this.products', this.products.value());
+		const product = { id: this.products.value()?.length || 0, name: this.query() };
+
+		this.products.update((products) => (products ? [product, ...products] : [product]));
+		axios.post('http://localhost:3000/api/products', { name: this.query() });
+	}
 }
